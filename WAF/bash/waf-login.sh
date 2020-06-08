fbeebe9a7c061605573fe7897901e6143e46722d
Michael S Collins <mercutio.viz@gmail.com>
2020-06-08 10:29:17 -0700

    Yay, figured out line-endings in VS Code.

--- WAF/bash/waf-login.sh
+++ WAF/bash/waf-login.sh
@@ -4,13 +4,18 @@
 #
 # Accepts username and password and WAF hostname/IP:port and prints out the token
 #
+# NOTE: do a "dot include" to have ${waftoken} populated in the bash env
+# Example:
+# ". ./waf-login.sh" at the CLI
+#
+
 host='51.143.38.93:8000'
 user='wafapiuser'
 pass='cloud2020'
 response=`curl -s -X POST http://${host}/restapi/v3.1/login -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"password\": \"${pass}\", \"username\": \"${user}\"}"`
-#
+
 #echo "${response}"
-#
+
 waftoken=`echo ${response} | jq -r '.' | grep 'token' | cut -b "12-" | sed -e 's/n"/n:"/'`
 echo "waftoken=${waftoken}"
 export "waftoken=${waftoken}"
