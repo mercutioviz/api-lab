a6c654c83af42ffd27fd0044609d286640300987
Michael S Collins <mercutio.viz@gmail.com>
2020-06-19 14:50:54 -0700

    remove debug code

--- WaaS/bash/get-waas-applications.sh
+++ WaaS/bash/get-waas-applications.sh
@@ -9,7 +9,7 @@ host='api.waas.barracudanetworks.com'
 apiurl='/v2/waasapi/applications/'
 method='GET'
 headers='-H "accept: application/json" -H "Content-Type: application/json"'
-response=`curl -s -X ${method} https://${host}${apiurl}/6592/ ${headers} -H "auth-api: ${waastoken}"`
+response=`curl -s -X ${method} https://${host}${apiurl} ${headers} -H "auth-api: ${waastoken}"`
 
 ## DEBUG 
 #echo "curl -s -X ${method} https://${host}${apiurl} ${headers} ${auth}"
