427d63b14a207cc39602b1e32bd639213ff861e3
Michael S Collins <mercutio.viz@gmail.com>
2020-08-11 10:22:01 -0700

    add list waas apps script

---
+++ WaaS/bash/list-waas-applications.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# list-waas-applications.sh
+#
+# Lists applications for the current user/account_id
+#  Shows id and name
+#
+
+host='api.waas.barracudanetworks.com'
+apiurl='/v2/waasapi/applications/'
+method='GET'
+headers='-H "accept: application/json" -H "Content-Type: application/json"'
+response=`curl -s -X ${method} https://${host}${apiurl} ${headers} -H "auth-api: ${waastoken} | jq '.results[] | {"id": .id, "name": .name}'"`
+
+echo "${response}"
+
