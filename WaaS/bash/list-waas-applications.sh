2a6a89ef1d7c2d2e879b8e5e69af5243c23b0ea8
Michael S Collins <mercutio.viz@gmail.com>
2020-08-11 10:24:21 -0700

    fix typo

--- WaaS/bash/list-waas-applications.sh
+++ WaaS/bash/list-waas-applications.sh
@@ -10,7 +10,7 @@ host='api.waas.barracudanetworks.com'
 apiurl='/v2/waasapi/applications/'
 method='GET'
 headers='-H "accept: application/json" -H "Content-Type: application/json"'
-response=`curl -s -X ${method} https://${host}${apiurl} ${headers} -H "auth-api: ${waastoken} | jq '.results[] | {"id": .id, "name": .name}'"`
+response=`curl -s -X ${method} https://${host}${apiurl} ${headers} -H "auth-api: ${waastoken}" | jq '.results[] | {"id": .id, "name": .name}'`
 
 echo "${response}"
 
